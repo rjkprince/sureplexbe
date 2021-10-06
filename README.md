19789d18c55236f602276acf3722b8ee966570a2
Rajesh kumar <prince.rk.cool@gmail.com>
2021-10-07 02:22:24 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Steps to run the application.
+
+1. clone the repository
+2. install all the dependencies using `yarn install` or `npm install`
+3. run `yarn start` and see if the application is live on localhost port 4000
+
+List of endpoints.
+
+1. /getPeopleInRange: get the list of people whose balance is in given range.
+2. /getFriends: get list of friends of the people depending on activity and balance.
---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const people = require("./people 2.json");
+const app = express();
+const cors = require("cors");
+const port = 4000;
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
+
+app.post("/getPeopleInRange", (req, res) => {
+  const body = req.body;
+  const range = body?.range;
+  if (range) {
+    if (Array.isArray(range) && range.length >= 2) {
+      const min = Number(range[0]);
+      const max = Number(range[1]);
+      if (isNaN(min) || isNaN(max))
+        return res
+          .status(400)
+          .send("Invalid range format. Range should be an array of 2 numbers");
+      const filteredPeople = people.filter((p) => {
+        const balance = parseFloat(p.balance.replace(",", "").replace("$", ""));
+        return balance > min && balance < max;
+      });
+      return res.status(200).send(filteredPeople);
+    } else
+      return res
+        .status(400)
+        .send("Invalid range format. Range should be an array of 2 numbers");
+  } else return res.status(400).send("range is not defined");
+});
+
+app.post("/getFriends", (req, res) => {
+  const body = req.body;
+  let { active, balance } = body;
+  if (active === undefined || balance === undefined)
+    return res.status(400).send("active or balance is not defined");
+  active = active === "true" ? true : active === "false" ? false : active;
+  balance = Number(balance);
+
+  if (typeof active === "boolean" && !isNaN(balance)) {
+    const filteredPeople = people.filter((p) => {
+      const pBalance = parseFloat(p.balance.replace(",", "").replace("$", ""));
+      return p.isActive === active && pBalance < balance;
+    });
+    const friends = [];
+    filteredPeople.forEach((ppl) => {
+      ppl.friends.forEach((frnd) => friends.push(frnd));
+    });
+    return res.status(200).send(friends);
+  } else
+    return res
+      .status(400)
+      .send(
+        "Invalid active or balance format. Active should be a boolean and balance should be a number."
+      );
+});
+
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
